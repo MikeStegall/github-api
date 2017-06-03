4ac6c86bc84554fc7dff81bed73a5c0d659e064b
MikeStegall <mestegall5@gmail.com>
2017-06-02 21:30:30 -0500

    Added a loading state

--- src/SearchName.js
+++ src/SearchName.js
@@ -5,7 +5,7 @@ function SearchUserName () {
     <div className='search-user-name'>
       <input type='text' name='username' placeholder='Enter GitHub Username' />
       <br />
-      <button type='button' className='btn-search-name'>Search User Name</button>
+      <input type='submit' value='Search' className='btn-search-name' />
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -15,8 +15,17 @@ const GITHUBURL = `https://api.github.com/users/MikeStegall` + _token
 
 let mainObj
 
+let loadingState = {
+  isLoading: true
+}
+
+function isLoading () {
+  return <h1>Loading...</h1>
+}
+
 function fetchGitHubData (data) {
   mainObj = data
+  loadingState.isLoading = false
   renderNow()
 }
 
@@ -26,18 +35,32 @@ function fetchFail () {
 $.getJSON(GITHUBURL).done(fetchGitHubData).fail(fetchFail)
 
 function RenderHTML (props) {
-  return (<div>
-    {GitHubName(props)}
-    {GitHubPic(props)}
-    {ProfileLinks(props)}
-    {ProfileLocation(props)}
-    {IsHireable(props)}
-    {SearchUserName()}
-  </div>)
+  if (loadingState.isLoading) {
+    console.log(loadingState.isLoading)
+    return (
+      <div className='loading'>
+        {isLoading()}
+      </div>
+    )
+  } else {
+    console.log(loadingState.isLoading)
+    return (
+      <div className='component'>
+        {GitHubName(props)}
+        {GitHubPic(props)}
+        {ProfileLinks(props)}
+        {ProfileLocation(props)}
+        {IsHireable(props)}
+        {SearchUserName()}
+      </div>
+    )
+  }
 }
 function App () {
   return RenderHTML(mainObj)
 }
 function renderNow () {
+  console.log('loading state in render function ' + loadingState.isLoading)
   ReactDOM.render(App(), document.getElementById('root'))
 }
+renderNow()
